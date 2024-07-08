f139ad487388daa73c22f24e0b6171df1161f2ce
QuickLike <QuickLikerfree@gmail.com>
2024-07-08 13:57:47 +0300

    users/admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Subscription
 
 
-UserAdmin.fieldsets = (
-    ('Extra Fields', {'fields': ('username', 'email',)}),
-)
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Fields', {'fields': ('avatar',)}),
+    )
 
-admin.site.register(CustomUser, UserAdmin)
+
+@admin.register(Subscription)
+class SubscriptionsAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'subscribe_on',
+    )
+    search_fields = (
+        'user',
+        'subscribe_on',
+    )
+    list_filter = (
+        'user',
+        'subscribe_on',
+    )
+    list_display_links = (
+        'user',
+    )
